10889a3b0d52575b96603e8af775d261d5db4967
Ashu-spec <christianashish050@gmail.com>
2022-09-20 12:59:13 +0530

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './assets/*.liquid',
+    './config/*.json',
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
